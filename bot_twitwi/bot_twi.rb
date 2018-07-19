83cc736271c3b983c3e476c7ef3138c5ce4b6cc4
Gwekkeo <gwendoline.cassand@gmail.com>
2018-07-19 11:45:16 +0200

    ajout

--- bot_twitwi/bot_twi.rb
+++ bot_twitwi/bot_twi.rb
@@ -2,10 +2,19 @@ load 'choperHandleInSpread.rb'
 load 'followTwitter.rb'
 
 
-# POUR TAB HANDLES DU SPREADSHEET
-objHandle = TakeHandlesInSpread.new
-tab_handle = objHandle.choper
 
-# POUR FOLLOW
-bot = FollowTwitter.new
-bot.follow(tab_handle)
+def go_bot
+	# POUR SCRAPPER LES @ DES MAIRIES
+	puts "[.........................]"
+
+	# POUR TAB HANDLES DU SPREADSHEET
+	objHandle = TakeHandlesInSpread.new
+	tab_handle = objHandle.choper
+
+	# POUR FOLLOW
+	bot = FollowTwitter.new
+	bot.follow(tab_handle)
+
+end
+
+go_bot
