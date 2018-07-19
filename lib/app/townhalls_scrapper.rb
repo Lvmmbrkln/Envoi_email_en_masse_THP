0fffbc30cdc5c3c77566ccfe9a42727082fbf997
Lvmmbrkln <j.degueltzl@gmail.com>
2018-07-19 18:24:58 +0200

    ajout fichiers jerem

--- lib/app/townhalls_scrapper.rb
+++ lib/app/townhalls_scrapper.rb
@@ -7,11 +7,17 @@ require 'google_drive'
 session = GoogleDrive::Session.from_config("townhalls.json")
 
 
-@root           = "http://annuaire-des-mairies.com"
-@url_reunion    = "http://annuaire-des-mairies.com/reunion.html"
-@url_guyane     = "http://annuaire-des-mairies.com/guyane.html"
-@url_martinique = "http://annuaire-des-mairies.com/martinique.html"
+  @url_reunion    = "http://annuaire-des-mairies.com/reunion.html"
+  @url_guyane     = "http://annuaire-des-mairies.com/guyane.html"
+  @url_martinique = "http://annuaire-des-mairies.com/martinique.html"
 
+class Scrapper
+  attr_accessor :root, :url_reunion, :url_guyane, :url_martinique, :tab
+
+def initialize
+  @root = "http://annuaire-des-mairies.com"
+  @tab  = []
+end
 ######################################################recupere les mails
 def get_the_email(url)
 	#ouverture de l'url du ste de la ville
@@ -30,23 +36,27 @@ end
 #############################################################recupere les urls
 def get_all_the_urls(lien)
 	#Ouverture de l'url de l'annuaire des mairies du val d'oise
-	doc = Nokogiri::HTML(open(lien))
-  # doc = Nokogiri::HTML(open(@url_guyane))
-  # doc = Nokogiri::HTML(open(@url_martinique))
-	doc.css(".lientxt").each do |link|
-		#Scrapping de l'url de chaque mairie
-		url = @root + link["href"][1..-1].to_s###decoupe le premir caractere de la chaine
-		#Scrapping du nom de la ville
-		ville_name   = link.text
-		#Scrapping de l'adresse mail de la mairie
-		ville_email  = get_the_email(url)
+  doc = Nokogiri::HTML(open(lien))
+  doc.css(".lientxt").each do |link|
+    url = @root + link["href"][1..-1].to_s ###decoupe le premier caractere de la chaine
+    ville_name   = link.text
+    ville_email  = get_the_email(url)
     ville_postal = get_the_postal(url)
-		#Le tout stoqué dans un hash
-		ville = {:name => ville_name, :email => ville_email, :postal => ville_postal}
-		puts  ville
+    @ville = {:name => ville_name.downcase.capitalize, :email => ville_email, :postal => ville_postal}
+    @tab   << @ville
   end
 end
 
- get_all_the_urls(@url_reunion)
- get_all_the_urls(@url_guyane)
- get_all_the_urls(@url_martinique)
+def save()
+  puts @tab
+  File.open("townhalls.json","w") do |i|
+    i.write(@tab.to_json)
+  end
+end
+end
+
+scrap = Scrapper.new
+scrap.get_all_the_urls(@url_guyane)
+scrap.get_all_the_urls(@url_reunion)
+scrap.get_all_the_urls(@url_martinique)
+scrap.save
